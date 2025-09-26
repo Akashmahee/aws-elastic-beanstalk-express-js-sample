50bcee848bcbdf253687686b506fd9143d2a84aa
Akash <iamakashmahesh@gmail.com>
2025-09-26 15:15:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World Akash here How are you guys!'));
+app.get('/', (req, res) => res.send('Hello World Akash here How are you guys from manual stage is enabled!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
